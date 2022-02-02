b4f4715fc86a48feef8a31ccd62bb41ecd9fcc5b
Chelsey_Leonia <ChelseyLeonia@Cleonias-MacBook-Pro.local>
2022-02-02 19:00:25 +0300

    cleonia

--- draw_map.c
+++ draw_map.c
@@ -4,18 +4,35 @@ void	draw_map_square(t_data *img, int y, int x, int size, int color)
 {
 	int i;
 	int j;
-	int size;
 
 	i = 0;
-	size = CEL_SIZE;
 	while (i < size)
 	{
 		j = 0;
 		while (j < size)
 		{
 			if (i == 0 || j == 0 || i == size - 1 || j == size - 1)
-				new_color = 0x000000;
-			my_mlx_pixel_put(img, x * CEL_SIZE + i, y * CEL_SIZE + j, new_color);
+				my_mlx_pixel_put(img, x * CEL_SIZE + i, y * CEL_SIZE + j, 0x000000);
+			else
+				my_mlx_pixel_put(img, x * CEL_SIZE + i, y * CEL_SIZE + j, color);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	draw_player_ray(t_all *all, int x, int y)
+{
+	int i;
+	int j;
+
+	i = y - 1;
+	while (i < y + 1)
+	{
+		j = x - 1;
+		while (j < x + 1)
+		{
+			my_mlx_pixel_put(&all->img, j, i, 0xFF0000);
 			j++;
 		}
 		i++;
@@ -24,24 +41,34 @@ void	draw_map_square(t_data *img, int y, int x, int size, int color)
 
 void	draw_player(t_all *all)
 {
+	// На экране
 	float x;
 	float y;
+	// На карте
+	float map_x;
+	float map_y;
 	int	i;
+	int j;
 
-	x = all->player.pos_x;
-	y = all->player.pos_y;
-	i = 0;
-	while (++i < 5)
+	x = 10 * CEL_SIZE - CEL_SIZE / 2;
+	y = 10 * CEL_SIZE - CEL_SIZE / 2;
+
+	map_x = all->player.pos_x;
+	map_y = all->player.pos_y;
+
+	while (map_x < 20 && map_y < 20 && map_x > 0 && map_y > 0)
 	{
-		if (all->map.map[(int)x][(int)y] == '1')
+		if (all->map.map[(int)map_x][(int)map_y] == '1')
 			break;
-		if (i == 1)
-			draw_square(&all->img, y, x, 0x000000);
-		else
-			draw_square(&all->img, y, x, 0xFF00FF);
+		map_x += all->player.dir_x;
+		map_y += all->player.dir_y;
+		if (x > 0 && y > 0)
+			// draw_player_ray(all, x, y);
 		x += all->player.dir_x;
 		y += all->player.dir_y;
 	}
+	// Начальная точка игрока
+	draw_map_square(&all->img, 9, 9, CEL_SIZE, 0xFF0000);
 }
 
 void	draw_map(t_all *all) // [width][height]
@@ -53,7 +80,7 @@ void	draw_map(t_all *all) // [width][height]
 	int j;
 
 	// рисует фон карты
-	// draw_map_square(&all->img, 0, 0, map_width * CEL_SIZE, map_color);
+	draw_map_square(&all->img, 0, 0, map_width * CEL_SIZE, map_color);
 
 	i = -1;
 	int y = all->player.pos_y - map_height / 2 - 1;
@@ -68,11 +95,12 @@ void	draw_map(t_all *all) // [width][height]
 		{
 			j++;
 			x++;
-			// if (x >= 0 && y >= 0 && x < all->map.width && y < all->map.height
-			// 	&& all->map.map[x][y] == '1')
-				// draw_map_square(&all->img, i, j, CEL_SIZE, 0xFFFFFF);
+			if (x >= 0 && y >= 0 && x < all->map.width && y < all->map.height
+				&& all->map.map[x][y] == '1')
+				draw_map_square(&all->img, i, j, CEL_SIZE, 0xFFFFFF);
 		}
 	}
 	// Рисует игрока
 	// draw_map_square(&all->img, 10, 10, 3, 0xFF00FF);
+	draw_player(all);
 }
